d4d1de71d64267fd5ecb9dbd8ba9e8f99fc5649e
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-09-21 15:53:51 +0100

    models and some security configurations

--- src/main/java/com/ecommerce/services/BuyerServices.java
+++ src/main/java/com/ecommerce/services/BuyerServices.java
@@ -1,12 +1,13 @@
 package com.ecommerce.services;
 
 import com.ecommerce.data.models.*;
+import com.ecommerce.web.exceptions.AccountCreationException;
 
 import java.util.List;
 
 
 public interface BuyerServices {
-    CustomerCreationDto addAccount(CustomerCreationDto customerCreationDto);
+    CustomerCreationDto addAccount(CustomerCreationDto customerCreationDto) throws AccountCreationException;
     List<Customer> findSellerByName(String sellerName);
     List<Product> findProductsByName(String productName);
     List<Product> findProductsBySellerName(String sellerName);
--- src/main/java/com/ecommerce/services/BuyerServicesImpl.java
+++ src/main/java/com/ecommerce/services/BuyerServicesImpl.java
@@ -2,18 +2,37 @@ package com.ecommerce.services;
 
 import com.ecommerce.data.models.*;
 import com.ecommerce.data.repository.BuyerRepository;
+import com.ecommerce.web.exceptions.AccountCreationException;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@Slf4j
 public class BuyerServicesImpl implements BuyerServices {
 
     @Autowired
     BuyerRepository buyerRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
-    public CustomerCreationDto addAccount(CustomerCreationDto customerCreationDto) {
+    public CustomerCreationDto addAccount(CustomerCreationDto customerCreationDto) throws AccountCreationException {
+        log.info("The Customer Information received is -->{}",customerCreationDto);
+        if(customerCreationDto.getEmailAddress().isBlank()){
+            throw new AccountCreationException("Account Name cannot be blank, please enter a valid name");
+        }
+        if(customerCreationDto.getPassword().isBlank()){
+            throw new AccountCreationException("Account Name cannot be blank, please enter a valid name");
+        }
+
+        Buyer buyer= new Buyer();
+        modelMapper.map(customerCreationDto,buyer);
+        buyerRepository.save(buyer);
+
         return null;
     }
 
