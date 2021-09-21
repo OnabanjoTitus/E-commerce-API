27ca2bfc50015b8ea07c0cf9a5853dc8d43247a3
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-09-21 14:55:04 +0100

    models and some security configurations

--- src/main/java/com/ecommerce/data/models/ProductDto.java
+++ src/main/java/com/ecommerce/data/models/ProductDto.java
@@ -3,8 +3,6 @@ package com.ecommerce.data.models;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.Column;
-import java.math.BigDecimal;
 @Data
 public class ProductDto {
     private String productName;
