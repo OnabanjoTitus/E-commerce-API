4ce60da896de15b5e46b6837e42294ad92cc74af
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-10-04 21:48:12 +0100

    permissions

--- src/main/java/com/ecommerce/security/config/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/security/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable().
                 authorizeRequests()
-                .antMatchers("/api/v1/customer/buyer/buyerRegistration")
+                .antMatchers("/api/v1/customer/buyer/***","/api/v1/customer/seller/***")
                 .permitAll()
                 .anyRequest()
                 .authenticated().and()
