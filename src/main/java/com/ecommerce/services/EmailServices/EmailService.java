0869b9084e378ec25e59c8ba77dbc31af1b75f22
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-10-16 14:48:21 +0100

    mail impl

--- src/main/java/com/ecommerce/services/EmailServices/EmailService.java
+++ src/main/java/com/ecommerce/services/EmailServices/EmailService.java
@@ -1,13 +1,16 @@
-//package com.ecommerce.services.EmailServices;
-//
-//import com.ecommerce.data.models.Product;
-//import com.example.famousventureempire.data.model.Product;
-//import org.springframework.stereotype.Service;
-//
-//
-//import java.util.List;
-//
-//@Service
-//public interface EmailService {
-//    void sendMail(List<Product> productList, String PhoneNumber) throws MessagingException;
-//}
+package com.ecommerce.services.EmailServices;
+
+import com.ecommerce.data.models.Product;
+
+import com.ecommerce.dtos.BuyerDto;
+import com.ecommerce.dtos.SellerDto;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public interface EmailService {
+    void sendMail(BuyerDto buyerDto);
+    void sendMail(SellerDto sellerDto);
+}
